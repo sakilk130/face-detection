49d8d172ba3aaba11c2be47fbc6d15244a4498b6
sakilk130 <sakilk130@gmail.com>
2022-01-23 00:06:28 +0600

    feat: image link form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
