f964523bd36866da3d52c97d67cc5525da1118b6
sakilk130 <sakilk130@gmail.com>
2022-01-23 00:34:18 +0600

    feat: rank & particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 500,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particlesOption} className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+
       {/* <FaceRecognition /> */}
     </div>
   );
